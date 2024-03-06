0a9fa8c0ab5761bf86a6c8d7b8e5c992d9edb5bc
gabrielgalvao0 <gabrielgalvao064@gmail.com>
2024-03-06 10:56:38 -0300

    codigo corrigido

--- cadastro.php
+++ cadastro.php
@@ -1,11 +1,11 @@
 <?php
 $login = $_POST['login'];
 $senha = MD5($_POST['senha']);
-$connect = mysql_connect('nome_do_servidor','nome_do_usuario','senha');
-$db = mysql_select_db('nome_do_banco_de_dados');
+$connect = mysqli_connect('localhost','root','');
+$db = mysqli_select_db('$connect,login');
 $query_select = "SELECT login FROM usuarios WHERE login = '$login'";
-$select = mysql_query($query_select,$connect);
-$arrayb= mysql_fetch_array($select);
+$select = mysqli_query($connect,query_select);
+$arrayb= mysqli_fetch_array($select);
 $logarray = $array['login'];
 
   if($login == "" || $login ==null){
@@ -21,7 +21,7 @@ $logarray = $array['login'];
 
     }else{
         $query = "INSERT INTO usuarios (login,senha) VALUES ('$login','$senha')";
-        $insert = mysql_query($query,$connect);
+        $insert = mysqli_query($connect,$query);
 
         if($insert){
             echo"<script language='javascript' type='text/javascript'>
